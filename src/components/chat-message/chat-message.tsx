72a7c81cb958438298bc2ba599af9901c4ba7476
Gokhan Kurt <krtgkn@gmail.com>
2020-06-04 14:45:03 +0300

    chat page style fixes

--- src/components/chat-message/chat-message.tsx
+++ src/components/chat-message/chat-message.tsx
@@ -8,6 +8,11 @@ export interface ChatMessageProps {
 
 export default function ChatMessage(props: ChatMessageProps) {
   return <li className={cx(styles.chatMessage, { 'right-aligned': props.rightAligned })}>
+    <div className="sender-info">
+      <em className="time">{'22:12'}</em>
+      <span className="separator">&nbsp;&bull;&nbsp;</span>
+      <span className="name">{'Person name'}</span>
+    </div>
     <div className="bubble-container">
       <div className="bubble-arrow"></div>
       <div className="bubble">
